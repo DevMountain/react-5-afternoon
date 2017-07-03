e4971612409701daef75cac0b9584bb5440d6b9d
atrevisanut <atrevisanut@gmail.com>
2017-07-03 11:48:35 -0600

    styles

--- src/components/NextBtn/NextBtn.js
+++ src/components/NextBtn/NextBtn.js
@@ -4,11 +4,13 @@ import { Link } from 'react-router-dom'
 class NextBtn extends Component {
     render(){
         return(
-            <div>
-                Home Loan Wizard <br />
-                <Link to="/wOne" name={this.props.user}>
-                    <button>Get Started</button>
-                </Link>
+            <div className="parent-div">
+                <div className="vert-align">
+                    <p className="home-text">Home Loan Wizard</p> <br />
+                    <Link to="/wOne" name={this.props.user}>
+                        <button>Get Started</button>
+                    </Link>
+                </div>
             </div>
         )
     }
--- src/components/WizardOne/WizardOne.js
+++ src/components/WizardOne/WizardOne.js
@@ -5,31 +5,32 @@ class WizardOne extends Component {
 
     render(){
         return(
-            <div>
-                What type of loan will you be needing? <br />
-            
-                <select onChange={this.props.handleLoanType}>
-
-                    <option type="text" value="homePurchase" >Home Purchase</option>
-                    <option type="text" value="refinance" >Refinance</option>
-                    <option type="text" value="homeEquity" >Home Equity loan/line</option>
-
-                </select> <br/>
-
-                What type of property are you purchasing? <br />
-
-                <select onChange={this.props.handlePropType}>
-
-                    <option value="singleFamHome">Single Family Home</option>
-                    <option value="townHome">Townhome</option>
-                    <option value="condo">Condo</option>
-                    <option value="multiFam">Multi Family Dwelling</option>
-                    <option value="mobileHome">Mobile Home</option>
-
-                </select>
+            <div className="parent-div">
+                <div className="vert-align">
+                    <p>What type of loan will you be needing?</p> <br />
                 
-                <Link to="/wTwo"><button > Next </button></Link>
+                    <select onChange={this.props.handleLoanType}>
 
+                        <option type="text" value="homePurchase" >Home Purchase</option>
+                        <option type="text" value="refinance" >Refinance</option>
+                        <option type="text" value="homeEquity" >Home Equity loan/line</option>
+
+                    </select> <br/>
+
+                    <p>What type of property are you purchasing?</p> <br />
+
+                    <select onChange={this.props.handlePropType}>
+
+                        <option value="singleFamHome">Single Family Home</option>
+                        <option value="townHome">Townhome</option>
+                        <option value="condo">Condo</option>
+                        <option value="multiFam">Multi Family Dwelling</option>
+                        <option value="mobileHome">Mobile Home</option>
+
+                    </select>
+                    
+                    <Link to="/wTwo"><button > Next </button></Link>
+                </div>
             </div>
         )
     }
