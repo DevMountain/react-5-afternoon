a09b84772706d281751be17812ae0e1e3ac4ebc5
atrevisanut <atrevisanut@gmail.com>
2017-07-03 15:51:05 -0600

    organizational change

--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-import store from './store'
-import { Provider } from 'react-redux'
 import registerServiceWorker from './registerServiceWorker';
 import './index.css';
 
+import store from './store'
+import { Provider } from 'react-redux'
 import { BrowserRouter } from 'react-router-dom';
 
 ReactDOM.render(
