4a62226409b3f4f268d3b839de7eff7654f84cff
atrevisanut <atrevisanut@gmail.com>
2017-07-03 12:26:45 -0600

    so many little changes

--- src/components/Finish/Finish.js
+++ src/components/Finish/Finish.js
@@ -1,18 +1,21 @@
 import React,  { Component } from 'react';
 import { Link } from 'react-router-dom';
+import { connect } from 'react-redux'; 
 
 class Finish extends Component {
+
     render(){
         return(
             <div className="parent-div">
-                <div className="vert-align">                   
-
-                    <p>Thank you for submitting your application, and loan consultant will be in touch with you shortly.</p>
-
+                <div className="vert-align">
+                    <p>Thank you for choosing Home Loan Wizard, a representative will be in touch with you shortly to talk about further steps.</p> <br />
+               
+                
                     <Link to="/"><button className="margin-btn"> Home </button></Link>
                 </div>
             </div>
         )
     }
 }
-export default Finish;
+
+export default Finish
