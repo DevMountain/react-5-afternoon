45c12901f0090597534a8c1bcf46655c1174b534
atrevisanut <atrevisanut@gmail.com>
2017-07-06 14:09:29 -0600

    organizational change

--- src/components/WizardEight/WizardEight.js
+++ src/components/WizardEight/WizardEight.js
@@ -6,7 +6,8 @@ class WizardEight extends Component {
     render(){
         return(
             <div className="parent-div">
-                    <div className="vert-align">                    <p>Have you had a bankruptcy or foreclosure in the past seven years? </p><br />
+                    <div className="vert-align">                    
+                        <p>Have you had a bankruptcy or foreclosure in the past seven years? </p><br />
                     <div className="row">
                         <Link to="/wNine"><button value="Has never been in bankruptcy" onClick={this.props.updateHistory}>No</button></Link>
                         <Link to="/wNine"><button value="Has had bankruptcy before" onClick={this.props.updateHistory}>Bankruptcy</button></Link>
--- src/components/WizardSeven/WizardSeven.js
+++ src/components/WizardSeven/WizardSeven.js
@@ -6,7 +6,10 @@ class WizardSeven extends Component {
     render(){
         return(
             <div className="parent-div">
-                <div className="vert-align">                    <p>Estimate your credit score</p> <br />
+                <div className="vert-align">                    
+                    
+                    <p>Estimate your credit score</p> <br />
+                    
                     <div className="row">
                         <Link to="/wEight"><button onClick={this.props.updateCreditE}>Excellent</button></Link>
                         <Link to="/wEight"><button onClick={this.props.updateCreditG}>Good</button></Link>
