8b3e629c93f4e606141f5f8bf829e9237a1d172b
= <webdev.jameslemire@gmail.com>
2017-08-08 10:11:02 -0600

    JL readme-updates - Step 12 Wizard 9 solution.

--- README.md
+++ README.md
@@ -2428,6 +2428,54 @@ export default connect( mapStateToProps, { updateHistory } )( WizardEight );
 <summary> <code> src/components/WizardNine/WizardNine.js </code> </summary>
 
 ```js
+import React,  { Component } from 'react';
+import { Link } from 'react-router-dom';
+
+import { connect } from 'react-redux';
+import { updateAddress1, updateAddress2, updateAddress3 } from '../../ducks/reducer';
+
+class WizardNine extends Component {
+  render() {
+    const { updateAddress1, updateAddress2, updateAddress3 } = this.props;
+
+    return (
+      <div className="parent-div">
+        <div className="vert-align">
+            <p>What is your address?</p> <br />
+
+            <input type="text" placeholder="Line One" onChange={ ( e ) => updateAddress1( e.target.value ) } />
+            <input type="text" placeholder="Line Two" onChange={ ( e ) => updateAddress2( e.target.value ) } />
+            <input type="text" placeholder="Line Three" onChange={ ( e ) => updateAddress3( e.target.value ) } />
+        
+            <Link to="/wTen">
+              <button className="margin-btn"> Next </button>
+            </Link>
+        </div>
+      </div>
+    )
+  }
+}
+
+function mapStateToProps( state ) {
+  const { addressOne, addressTwo, addressThree } = state;
+
+  return {
+    addressOne,
+    addressTwo,
+    addressThree
+  };
+}
+
+export default connect( mapStateToProps, { updateAddress1, updateAddress2, updateAddress3 } )( WizardNine );
+```
+
+</details>
+
+<details>
+
+<summary> <code> src/components/WizardTen/WizardTen.js </code> </summary>
+
+```js
 
 ```
 
--- src/components/WizardNine/WizardNine.js
+++ src/components/WizardNine/WizardNine.js
@@ -1,25 +1,39 @@
 import React,  { Component } from 'react';
 import { Link } from 'react-router-dom';
 
+import { connect } from 'react-redux';
+import { updateAddress1, updateAddress2, updateAddress3 } from '../../ducks/reducer';
+
 class WizardNine extends Component {
+  render() {
+    const { updateAddress1, updateAddress2, updateAddress3 } = this.props;
 
-    render(){
-        return(
-            <div className="parent-div">
-                <div className="vert-align">                    
-                    
-                    <p>What is your address?</p> <br />
+    return (
+      <div className="parent-div">
+        <div className="vert-align">
+            <p>What is your address?</p> <br />
 
-                    <input type="text" placeholder="Line One" onChange={this.props.updateAddLineOne}/>
-                    <input type="text" placeholder="Line Two" onChange={this.props.updateAddLineTwo}/>
-                    <input type="text" placeholder="Line Three" onChange={this.props.updateAddLineThree}/>
-                
-                    
-                    <Link to="/wTen"><button className="margin-btn"> Next </button></Link>
-                </div>
-            </div>
-        )
-    }
+            <input type="text" placeholder="Line One" onChange={ ( e ) => updateAddress1( e.target.value ) } />
+            <input type="text" placeholder="Line Two" onChange={ ( e ) => updateAddress2( e.target.value ) } />
+            <input type="text" placeholder="Line Three" onChange={ ( e ) => updateAddress3( e.target.value ) } />
+        
+            <Link to="/wTen">
+              <button className="margin-btn"> Next </button>
+            </Link>
+        </div>
+      </div>
+    )
+  }
 }
 
-export default WizardNine;
+function mapStateToProps( state ) {
+  const { addressOne, addressTwo, addressThree } = state;
+
+  return {
+    addressOne,
+    addressTwo,
+    addressThree
+  };
+}
+
+export default connect( mapStateToProps, { updateAddress1, updateAddress2, updateAddress3 } )( WizardNine );
