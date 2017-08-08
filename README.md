70a69bca48a4bb02dd5da2d50dac7b68f0c583a3
= <webdev.jameslemire@gmail.com>
2017-08-08 09:32:46 -0600

    JL readme-updates - Step 12 Wizard 5 solution.

--- README.md
+++ README.md
@@ -2222,7 +2222,54 @@ Open the remaining `wizard` components ( Wizard 5-11 ) and update them to connec
 
 <summary> <code> src/components/WizardFive/WizardFive.js </code> </summary>
 
+```js
+import React,  { Component } from 'react';
+import { Link } from 'react-router-dom';
+
+import { connect } from 'react-redux';
+import { updateAgent } from '../../ducks/reducer';
+
+class WizardFive extends Component {
+  render() {
+    const { updateAgent } = this.props;
+
+    return (
+      <div className="parent-div">
+        <div className="vert-align">
+          <p>Are you currently working with a real estate agent?</p> <br />
+          <div className="row">
+            <Link to="/wSix">
+              <button onClick={ () => updateAgent( true ) }>Yes</button>
+            </Link>
+
+            <Link to="/wSix">
+              <button onClick={ () => updateAgent( false ) }>No</button>
+            </Link>
+          </div>
+        </div>
+      </div>
+    )
+  }
+}
+
+function mapStateToProps( state ) {
+  const { realEstateAgent } = state;
+
+  return {
+    realEstateAgent
+  };
+}
+
+export default connect( mapStateToProps, { updateAgent } )( WizardFive );
 ```
+
+</details>
+
+<details>
+
+<summary> <code> src/components/WizardSix/WizardSix.js </code> </summary>
+
+```js
 
 ```
 
--- src/components/WizardFive/WizardFive.js
+++ src/components/WizardFive/WizardFive.js
@@ -1,22 +1,38 @@
 import React,  { Component } from 'react';
 import { Link } from 'react-router-dom';
 
+import { connect } from 'react-redux';
+import { updateAgent } from '../../ducks/reducer';
+
 class WizardFive extends Component {
+  render() {
+    const { updateAgent } = this.props;
 
-    render(){
-        return(
-            <div className="parent-div">
-                    <div className="vert-align">    
+    return (
+      <div className="parent-div">
+        <div className="vert-align">
+          <p>Are you currently working with a real estate agent?</p> <br />
+          <div className="row">
+            <Link to="/wSix">
+              <button onClick={ () => updateAgent( true ) }>Yes</button>
+            </Link>
 
-                    <p>Are you currently working with a real estate agent?</p> <br />
-                    <div className="row">
-                        <Link to="/wSix"><button onClick={this.props.realEstateAgentTrue}>Yes</button></Link>
-                        <Link to="/wSix"><button onClick={this.props.realEstateAgentFalse}>No </button></Link>
-                    </div>
-                </div>
-            </div>
-        )
-    }
+            <Link to="/wSix">
+              <button onClick={ () => updateAgent( false ) }>No</button>
+            </Link>
+          </div>
+        </div>
+      </div>
+    )
+  }
 }
 
-export default WizardFive;
+function mapStateToProps( state ) {
+  const { realEstateAgent } = state;
+
+  return {
+    realEstateAgent
+  };
+}
+
+export default connect( mapStateToProps, { updateAgent } )( WizardFive );
