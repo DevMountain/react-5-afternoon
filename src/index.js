1301b13a139d0fffc13d989f591bb3a97615c568
Nathan Klundt <nathanryan001@gmail.com>
2018-03-18 17:07:33 -0600

    Remove HashRouter
    This is included in the instructions for step 2. Not intended to compile until step 2.

--- src/index.js
+++ src/index.js
@@ -1,13 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { HashRouter } from 'react-router-dom';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 import './index.css';
 
-ReactDOM.render(
-  <HashRouter>
-    <App />
-  </HashRouter>, document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
 
