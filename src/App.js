605078bdbdd844c038e76a27443ab9d7f476cb01
Tyler Collier <tyler.collier+github@devmounta.in>
2018-05-28 15:52:44 -0700

    Remove unnecessary use of redux in App.js (Step 4 of README)

--- src/App.js
+++ src/App.js
@@ -1,28 +1,15 @@
 import React, { Component } from 'react';
 import './App.css';
 import router from './router'
-import { connect } from "react-redux";
-
 
 class App extends Component {
   render() {
     return (
       <div>
-    
-      {router}
-
+        {router}
       </div>
     );
   }
 }
 
-function mapStateToProps(state){
-  return{
-
-      state
-
-  }
-}
-
-export default connect(mapStateToProps)(App);
-
+export default App;
